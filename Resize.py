2a68b7fe2371114fb8366dcc5edb5747cd737fb7
Anaya S Rao <vinayassrao@gmail.com>
2020-07-30 13:52:31 +0530

    first commit

---
+++ Resize.py
@@ -0,0 +1,18 @@
+# plot dog photos from the dogs vs cats dataset
+from matplotlib import pyplot
+from matplotlib.image import imread
+# define location of dataset
+folder = 'train/'
+# plot first few images
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330+i+1)
+	# define filename
+	filename = folder + 'cat.' + str(i) + '.jpg'
+	# load image pixels
+	image = imread(filename)
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+pyplot.savefig('sdf.png')
+pyplot.show()
